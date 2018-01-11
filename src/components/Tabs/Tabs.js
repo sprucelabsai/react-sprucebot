d285cf1b64f6dfc3108e010e6a7e755521d46c83
Leela <lkkadiri@gmail.com>
2018-01-11 15:41:41 -0700

    Tabs can now be conditionaly rendered from the parent

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -8,33 +8,29 @@ export class Tabs extends Component {
 	constructor(props) {
 		super(props)
 
-		// normalize children for some checks
-		let children = props.children || []
-		if (!Array.isArray(children)) {
-			children = [children]
-		}
-
-		// default to first selected item
-		let selected = props.selected || 0
-
-		children.forEach((tab, idx) => {
-			// lets make sure they are all tab panes
-			if (!tab || !tab.type || tab.type.name !== 'TabPane') {
-				throw new Error(
-					"<Tabs> can only contain <TapPane>'s as children (none optional)"
-				)
-			}
-
-			if (tab.props.selected) {
-				selected = idx
-			}
-		})
-
 		this.state = {
-			selected: selected
+			selected: 0,
+			children: this.filterChildren(props.children)
 		}
 	}
-
+	filterChildren(children = this.props.children) {
+		// default to first selected item
+		let selected = this.props.selected || 0
+		return React.Children.map(children, (tab, idx) => {
+			if (tab !== null) {
+				// lets make sure they are all tab panes
+				if (!tab || !tab.type || tab.type.name !== 'TabPane') {
+					throw new Error(
+						"<Tabs> can only contain <TapPane>'s as children (none optional)"
+					)
+				}
+				if (tab.props.selected) {
+					this.setState({ selected: idx })
+				}
+				return tab
+			}
+		})
+	}
 	componentWillReceiveProps(nextProps) {
 		if (
 			typeof nextProps.selected === 'number' &&
@@ -57,18 +53,12 @@ export class Tabs extends Component {
 
 	render() {
 		const props = Object.assign({}, this.props)
-		let { children, className } = props
-		const { selected } = this.state
+		let { className } = props
+		const { children, selected } = this.state
 
 		delete props.children
 		delete props.onChange
 		delete props.className
-
-		// normalize children (which should only be tabpanes)
-		children = children || []
-		if (!Array.isArray(children)) {
-			children = [children]
-		}
 
 		// build tab labels and selected
 		let tabs = []
