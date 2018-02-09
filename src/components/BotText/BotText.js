c9ebdf20e49888d91d0d4c5fe5e8d5c558620523
Travis Rollins <kalikoze@gmail.com>
2018-02-09 16:08:16 -0700

    SB-688 Special characters now word wrap in BotText and List styled components.

--- src/components/BotText/BotText.js
+++ src/components/BotText/BotText.js
@@ -2,6 +2,10 @@ import styled from 'styled-components'
 
 const BotText = styled.p.attrs({
 	className: 'bot__text'
-})``
+})`
+	word-wrap: break-word;
+	overflow-wrap: break-word;
+	word-break: break-word;
+`
 
 export default BotText
